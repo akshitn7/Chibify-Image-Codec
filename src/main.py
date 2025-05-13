cebd8ac996a823912f8888f1d25455d3f7c1c6af
Akshit Negi <akshitn2707@gmail.com>
2025-05-13 18:00:26 +0530

    Changed name of the site

--- src/main.py
+++ src/main.py
@@ -3,21 +3,20 @@ import io
 import codec as cd
 
 st.set_page_config(page_title="Image Compression", layout="centered")
-st.title("📸 Huffman Image Compression")
+st.title("🐥 Chibify 🐥")
 
-tab1, tab2 = st.tabs(["📤 Compress Image", "📥 Decompress File"])
+tab1, tab2 = st.tabs(["🐣 Compress Image", "🐔 Decompress File"])
 
-#Compress
+# ========== Compress ==========
 with tab1:
     img_file = st.file_uploader("Upload Image", type="bmp", accept_multiple_files=False)
     if img_file and st.button("Compress"):
         compressed = cd.compress_image(img_file)
         st.success("File Compressed")
-        st.download_button(label="Download your compressed file", data=compressed, file_name= (img_file.name.split(".")[0] + ".huff"), mime="application/octet-stream")
-
-#Decompress        
+        st.download_button(label="Download your compressed file", data=compressed, file_name= (img_file.name.split(".")[0] + ".chibi"), mime="application/octet-stream")
+        
 with tab2:
-    compressed = st.file_uploader("Upload Image", type="huff", accept_multiple_files=False)
+    compressed = st.file_uploader("Upload Image", type="chibi", accept_multiple_files=False)
     if compressed and st.button("Decompress"):
         decompressed = cd.decompress_image(compressed)
         file = io.BytesIO()
